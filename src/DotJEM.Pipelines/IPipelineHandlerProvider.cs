d9fbf16d58644443f5a4db05bfb3db2c1f078df1
Jens Melgaard <admin@it-links.dk>
2021-10-20 14:01:45 +0200

    add cumment to interface that doesn't really belong.

--- src/DotJEM.Pipelines/IPipelineHandlerProvider.cs
+++ src/DotJEM.Pipelines/IPipelineHandlerProvider.cs
@@ -1,8 +1,9 @@
 ﻿namespace DotJEM.Pipelines
 {
-    /// <summary>
-    /// This is a marker interface for registering Pipeline handlers within Castle.
-    /// </summary>
+    // Note: 
+    // This is a marker interface for registering Pipeline handlers within IoC containers.
+    // Strictly speaking, this is not a concern for this project, so if we can find a suidable way for dependant projects to quickly wire
+    // this up, e.g. by using a factory or custom implementation of the collection, then deleting this would be fair game. 
     public interface IPipelineHandlerProvider
     {
     }
